1998236952c38551f38f215cd4401c0a06ac0a02
CatAvadani <catalinaavadani@gmail.com>
2022-12-09 00:25:42 +0100

    Setting the dependencies in gradle file

--- app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.triviaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.triviaapp.ui.theme.TriviaAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,29 +23,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TriviaAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-
-                }
+                MyTriviaApp()
             }
         }
     }
 }
 
 @Composable
-fun MyTriviaApp(){
-}
+fun MyTriviaApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Catalina",
+                style = TextStyle(
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 35.sp
+                )
+            )
+        }
 
+    }
+
+
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TriviaAppTheme {
-
+        MyTriviaApp()
     }
 }
